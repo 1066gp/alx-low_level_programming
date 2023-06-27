2c2900001274767a239901e73df58ec21bea6046
1066gp <mndebelenonhlanhla92@gmai.com>
2023-06-26 18:04:40 -0700

    0x05. C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * puts2 - function should print only one character out of a string 
+ * puts2 - function should print only one character out of a string
  * starting with the first one
  * @str: input
  * Return: print
@@ -18,7 +18,7 @@ void puts2(char *str)
 		longi++;
 	}
 	t = longi -1;
-	for (o = 0 ; o<= t ; o++)
+	for (o = 0 ; o <= t ; o++)
 	{
 		if (o % 2 == 0)
 	{
